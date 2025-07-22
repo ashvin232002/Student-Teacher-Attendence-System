9c11702b2f18e6297f262d135e7d04c79bd09e83
ashvin232002 <ashvinkumhar2310@gmail.com>
2025-07-22 14:44:13 +0530

    changes added

--- app/src/main/java/com/example/student_teacher_attendence_system/data/model/Attendence.kt
+++ app/src/main/java/com/example/student_teacher_attendence_system/data/model/Attendence.kt
@@ -1,12 +1,13 @@
 package com.example.student_teacher_attendence_system.data.model
 
+import com.google.firebase.firestore.DocumentReference
 import java.util.Date
 
 
 data class AttendanceModel(
     val id: String = "",
-    val class_id: String = "",
-    val teacher_id: String = "",
+    val class_ref: DocumentReference? = null,
+    val teacher_ref: DocumentReference? = null,
     val day: Date = Date(),
     val presence_count: Int = 0
 )
--- app/src/main/java/com/example/student_teacher_attendence_system/data/model/Class.kt
+++ app/src/main/java/com/example/student_teacher_attendence_system/data/model/Class.kt
@@ -1,8 +1,10 @@
 package com.example.student_teacher_attendence_system.data.model
 
+import com.google.firebase.firestore.DocumentReference
+
 data class ClassModel(
     val id: String = "",
     val name: String = "",
     val total_strength: Int = 0,
-    val teacher_id: String = ""
+    val teacher_ref: DocumentReference? = null
 )
